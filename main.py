872619d87f1adb48ac7406405f72e90183f00973
Erick Santos <rdserick@gmail.com>
2026-05-25 18:33:27 -0300

    removing logs from terminal

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.FileHandler("journey_agent.log"),
-        logging.StreamHandler(sys.stdout),
+        logging.FileHandler("journey_agent.log", encoding="utf-8"),
     ],
 )
 
